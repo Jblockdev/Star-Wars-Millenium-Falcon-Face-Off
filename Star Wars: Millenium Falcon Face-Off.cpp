84e0e634dac560bdbcf16de1e33758e5c63ffa05
Jblockdev <jeremyblock1@gmail.com>
2023-09-23 15:16:25 +0300

    Optimized if/else if conditionals, and debugged a misprint to the console based on user input for attacks chosen.

--- Star Wars: Millenium Falcon Face-Off.cpp
+++ Star Wars: Millenium Falcon Face-Off.cpp
@@ -38,43 +38,43 @@ Move get_move(Character character) {
     // Get the player's or AI's move choice
     Move chosen_move;
     int move_choice;
-    
+
     if (player_turn) {
         cout << "Select a move for " << character.name << ":" << endl;
         for (int i = 0; i < 3; i++) {
             cout << i << ". " << character.moves[i].name << endl;
         }
-        
+
         cin >> move_choice;
         chosen_move = character.moves[move_choice];
     } else {
         move_choice = rand() % 3;
         chosen_move = character.moves[move_choice];
-        
+
         cout << character.name << " used " << chosen_move.name << endl;
     }
-    
+
     return chosen_move;
 }
 
-bool is_game_over(Character &han, Character &lando) {
-    // Check if the game is over (i.e., either player has 0 or less health)
-    if (han.health <= 0) {
-        cout << "Lando Calrissian wins!" << endl;
-        cout << "Han Solo has " << 0 << " health remaining." << endl;
-        cout << "Lando Calrissian has " << lando.health << " health remaining." << endl;
+bool is_game_over(Character &user, Character &AI) {
+    // Check if the game is over (i.e., either user has 0 or less health)
+    if (user.health <= 0) {
+        cout << AI.name << " has defeated " << user.name << " and will keep the Millennium Falcon!" << endl;
+        cout << AI.name << " finished the battle with " << AI.health << "%" << " health left" << endl;
         return true;
-    } else if (lando.health <= 0) {
-        cout << "Han Solo wins!" << endl;
-        cout << "Lando Calrissian has " << 0 << " health remaining." << endl;
-        cout << "Han Solo has " << han.health << " health remaining." << endl;
+
+    } else if (AI.health <= 0) {
+        cout << user.name << " has defeated " << AI.name << " and is returning to Bespin in a new ride!" << endl;
+        cout << user.name << " finished the battle with " << user.health << "%" << " health left" << endl;
         return true;
+
     } else {
         return false;
     }
 }
 
-void game_loop(Character &player, Character &ai) {
+void game_loop(Character &user, Character &AI) {
     bool game_over = false;
 
     cout << "The game begins!" << endl << endl;
@@ -82,24 +82,23 @@ void game_loop(Character &player, Character &ai) {
     while (!game_over) {
         if (player_turn) {
             cout << "Your turn!" << endl;
-        } else {
-            cout << "AI's turn..." << endl;
+            cout << endl;
+            cout << user.name << " has " << user.health << "%" << " remaining." << endl;
         }
-
-        if (player_turn) {
-            cout << "Your health: " << player.health << endl;
-            cout << "AI's health: " << ai.health << endl;
+        else {
+            cout << "AI's turn..." << endl;
+            cout << endl;
+            cout << AI.name << " has " << AI.health << "%" << " remaining." << endl;
         }
 
         // Calculate and apply damage to the defender based on the attacker's selected move
-        Move move = get_move(player_turn ? player : ai);
+        Move move = get_move(player_turn ? user : AI);
 
         int damage = rand() % (move.max_damage - move.min_damage + 1) + move.min_damage;
-        (player_turn ? ai : player).health -= damage;
+        (player_turn ? AI : user).health -= damage;
 
-        cout << (player_turn ? player.name : ai.name) << " used " << move.name << " and dealt " << damage << " damage to " << (player_turn ? ai.name : player.name) << "." << endl;
-
-        game_over = is_game_over(player, ai);
+        cout << (player_turn ? user.name : AI.name) << " used " << move.name << " and dealt " << damage << "%" << endl;
+        game_over = is_game_over(user, AI);
 
         player_turn = !player_turn;
 
@@ -110,61 +109,81 @@ void game_loop(Character &player, Character &ai) {
 int main() {
     // Seed the random number generator
     srand(time(0));
-    
+
     // Initialize the characters
     Character han = {"Han Solo", han_moves, 100};
     Character lando = {"Lando Calrissian", lando_moves, 100};
-    
+
     // Display the intro and prompt the player to choose a character
-    cout << "\t\t\t\tSTAR WARS: Millenium Falcon Face-Off" << endl;
+    cout << "\t\t\t\tSTAR WARS: Millennium Falcon Face-Off" << endl;
     cout << endl;
-    cout << "The winner will keeps the Millenium Falcon!" << endl;
+    cout << "The last one standing keeps the Millennium Falcon!" << endl;
     cout << endl;
     cout << "Choose your character: " << endl;
     cout << endl;
     cout << "0: Han Solo " << endl;
+    cout << endl;
     cout << "1: Lando Calrissian " << endl;
     cout << endl;
-    
+
     int character_choice;
     cin >> character_choice;
-    
-    Character player;
-    Character ai;
-    
+
+    Character user;
+    Character AI;
+
     if (character_choice == 0) {
-        player = han;
-        ai = lando;
+        user = han;
+        AI = lando;
     } else {
-        player = lando;
-        ai = han;
+        user = lando;
+        AI = han;
     }
-    
-    cout << "You have chosen " << player.name << "." << endl;
+
+    cout << "You have chosen " << user.name << "." << endl;
 
     // Start the game loop
+    bool game_over = false;
     do {
         // Reset the health of both characters and the player turn flag
         han.health = 100;
         lando.health = 100;
         player_turn = true;
-        
-        game_loop(player, ai);
 
-        // Ask the player if they want to play again
+        game_loop(user, AI);
+
+        // Ask the user if they want to play again
         char answer;
         cout << "Do you want to play again? (y/n): ";
         cin >> answer;
         cout << endl;
-        
+
         if (answer == 'y' || answer == 'Y') {
-            // Start a new game
-            continue;
+            // Prompt the user to choose a new character
+            cout << "Choose your character: " << endl;
+            cout << endl;
+            cout << "0: Han Solo " << endl;
+            cout << endl;
+            cout << "1: Lando Calrissian " << endl;
+            cout << endl;
+
+            int character_choice;
+            cin >> character_choice;
+
+            if (character_choice == 0) {
+                user = {"Han Solo", han_moves, 100};
+                AI = {"Lando Calrissian", lando_moves, 100};
+            } else {
+                user = {"Lando Calrissian", lando_moves, 100};
+                AI = {"Han Solo", han_moves, 100};
+            }
+
+            game_over = false;
+            player_turn = true;
         } else {
-            // Exit the program
-            break;
+            cout << "Thanks for playing! May the force be with you - Always." << endl;
         }
-    } while (true);
-    
+    } while (!game_over);
+
     return 0;
 }
